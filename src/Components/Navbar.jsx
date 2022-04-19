669ac6ec8e5c6f1761c53b668a5d32c8adae9eb9
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-19 13:52:41 +0530

    added the responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Box, Flex, Spacer , Button, Heading} from "@chakra-ui/react";
+import { Box, Flex, Spacer , Button, Heading, HStack} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
-
+import { SiGnuprivacyguard } from "react-icons/si"
+import DrawerExample from './DrawerM'
 import { FaSignInAlt, FaHome } from "react-icons/fa"
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
-import { SiGnuprivacyguard } from "react-icons/si"
 const Navbar = () => {
   return (
     <>
@@ -29,6 +29,10 @@ const Navbar = () => {
         <Spacer />
         <Box>
             <ColorModeSwitcher />
+        </Box>
+        <HStack
+          display={{base : 'flex',lg : "flex", md : "none", sm : 'none'}}
+          >
             <Link to='/'>
                 <Button rightIcon={<FaHome />} colorScheme="teal" mr="4" size='md' variant='ghost'
                 >
@@ -43,7 +47,13 @@ const Navbar = () => {
             <Link to='/login'>
                 <Button rightIcon={<FaSignInAlt />} colorScheme="teal" mr="4" size='md' variant='ghost'>Log in</Button>
             </Link>
-        </Box>
+        </HStack>
+
+        <HStack 
+          display={{base : 'none',lg : "none", md : "flex", sm : 'flex'}}
+        >
+          <DrawerExample/>
+        </HStack>
       </Flex>
     </>
   );
