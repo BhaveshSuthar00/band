4ad20dffee413c06c73fa6feff8e11de8a3d4f47
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-25 09:30:11 +0530

    removed the dev tools from store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import {flatReducer } from './reducer'
 import ReduxThunk from "redux-thunk";
 export const store = createStore(
     flatReducer,
-    compose(
-        applyMiddleware(ReduxThunk), 
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(ReduxThunk)
 );
+    // compose(
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
 // console.log(store.getState())
